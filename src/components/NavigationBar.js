dcc515ca395dde5b28255a4b45eb2325eb9bfa48
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2022-08-17 18:24:00 +0700

    commit

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,32 +1,32 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function NavigationBar() {
   return (
-    <header class="navbar navbar-expand-md fixed-top" id="navbar">
-      <nav class="container container-md px-md-5 px-3">
-        <div class="border-animation">
-          <span class="inset"></span>
-          <span class="navbar-brand p-relative ps-3">IT DAYS 2022</span>
+    <header className="navbar navbar-expand-md fixed-top" id="navbar">
+      <nav className="container container-md px-md-5 px-3">
+        <div className="border-animation">
+          <span className="inset"></span>
+          <span className="navbar-brand p-relative ps-3">IT DAYS 2022</span>
         </div>
-        <button class="navbar-toggler float-md-end d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
-          <i class="bi bi-list text-light"></i>
+        <button className="navbar-toggler float-md-end d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
+          <i className="bi bi-list text-light"></i>
         </button>
 
-        <div class="offcanvas offcanvas-md offcanvas-bottom" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
-          <div class="offcanvas-header">
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
+        <div className="offcanvas offcanvas-md offcanvas-bottom" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
+          <div className="offcanvas-header">
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
           </div>
-          <div class="offcanvas-body">
-            <div class="navbar-nav text-center ms-auto">
-              <a class="nav-link" href="/">
+          <div className="offcanvas-body">
+            <div className="navbar-nav text-center ms-auto">
+              <Link className="nav-link" to="/">
                 Home
-              </a>
-              <a class="nav-link" href="/about/">
+              </Link>
+              <Link className="nav-link" to="/about/">
                 About
-              </a>
-              <a class="nav-link" href="/main-activity/">
+              </Link>
+              <Link className="nav-link" to="/main-activity/">
                 Main Activity
-              </a>
+              </Link>
             </div>
           </div>
         </div>
